c5ea74b88adc940409649d5d442f4559920b5dc7
MerryHao <mq6721@gmail.com>
2023-02-24 22:27:49 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.get('/users/login', (req, res) => {
     .catch((error) => { return res.status(422).json(error) })
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
 app.post('/users/login', (req, res) => {
   res.send('login')
 })
